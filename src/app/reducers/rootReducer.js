c5b3e0647a6b68b0dedcc577201116f45b34f0b1
Maxime <richard.maxime9@gmail.com>
2016-12-28 22:04:42 +0100

    create redux orm and init reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import {combineReducers} from "redux";
 
+import entitiesReducer from "./entitiesReducer";
 import tabReducer from "features/tabs/tabReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 
 const rootReducer = combineReducers({
+    entities : entitiesReducer,
     unitInfo : unitInfoReducer,
-    tabs : tabReducer,
+    tabs : tabReducer
 });
 
 export default rootReducer;
