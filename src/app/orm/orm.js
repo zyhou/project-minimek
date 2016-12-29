9a27bd205ea7668722728739936c7d94a755960f
zyhou <richard.maxime9@gmail.com>
2016-12-29 12:08:48 +0100

    define mech and mechdesign class model

--- src/app/orm/orm.js
+++ src/app/orm/orm.js
@@ -1,8 +1,10 @@
 import { ORM } from "redux-orm";
 
 import Pilot from "features/pilots/Pilot";
+import MechDesign from "features/mechs/MechDesign";
+import Mech from "features/mechs/Mech";
 
 const orm = new ORM();
-orm.register(Pilot);
+orm.register(Pilot, MechDesign, Mech);
 
 export default orm;
---
+++ src/features/mechs/Mech.js
@@ -0,0 +1,16 @@
+import { Model, fk } from "redux-orm";
+
+export default class Mech extends Model {
+    static get fields() {
+        return {
+            type: fk("MechDesign"),
+            pilot: fk("Pilot"),
+        };
+    }
+
+    static parse(mechData) {
+        return this.create(mechData);
+    }
+}
+
+Mech.modelName = "Mech"; 
