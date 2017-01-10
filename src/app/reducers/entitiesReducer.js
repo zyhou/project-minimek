8beef233da59919eb37b515ff3d0662ec468e56d
Maxime <richard.maxime9@gmail.com>
2017-01-10 21:50:02 +0100

    load mech and mech designs from sample data

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -4,17 +4,20 @@ import { DATA_LOADED } from "features/tools/toolConstants";
 
 import orm from "app/orm"
 
-const initialState = orm.getEmptyState();
+const initialState = orm.getDefaultState();
 
 export function loadData(state, payload) {
     // Create a Redux-ORM session from our entities "tables"
     const session = orm.session(state);
-    // Get a reference to the correct version of the Pilots class for this Session
-    const {Pilot} = session;
+    // Get a reference to the correct version of model classes for this Session
+    const {Pilot, MechDesign, Mech} = session;
 
-    const {pilots} = payload;
-    // Queue up creation commands for each pilot entry
+    const {pilots, designs, mechs} = payload;
+
+    // Queue up creation commands for each entry
     pilots.forEach(pilot => Pilot.parse(pilot));
+    designs.forEach(design => MechDesign.parse(design));
+    mechs.forEach(mech => Mech.parse(mech));
 
     // Apply the queued updates and return the updated "tables"
     return session.state;
@@ -22,4 +25,4 @@ export function loadData(state, payload) {
 
 export default createReducer(initialState, {
     [DATA_LOADED]: loadData
-}); 
+});
