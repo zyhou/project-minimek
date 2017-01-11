d4865f7cc17b654f393fd2485e977fb79266e781
zyhou <richard.maxime9@gmail.com>
2017-01-11 09:39:37 +0100

    update pilotdetails to be connected

--- src/features/pilots/Pilots/Pilots.jsx
+++ src/features/pilots/Pilots/Pilots.jsx
@@ -70,7 +70,6 @@ export class Pilots extends Component {
 
     render() {
         const {pilots = [], selectPilot, currentPilot} = this.props;
-        const currentPilotEntry = pilots.find(pilot => pilot.id === currentPilot) || {}
 
         return (
             <Segment>
@@ -86,7 +85,7 @@ export class Pilots extends Component {
                     <Grid.Column width={6}>
                         <Header as="h3">Pilot Details</Header>
                         <Segment >
-                            <PilotDetails pilot={currentPilotEntry} />
+                            <PilotDetails />
                         </Segment>
                     </Grid.Column>
                 </Grid>
