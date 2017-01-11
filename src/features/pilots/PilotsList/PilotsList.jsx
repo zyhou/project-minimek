b4a44c80fd9a419b8cf745bd4c74983f21fc8982
zyhou <richard.maxime9@gmail.com>
2017-01-11 09:48:09 +0100

    update pilotslist connected

--- src/features/pilots/PilotsList/PilotsList.jsx
+++ src/features/pilots/PilotsList/PilotsList.jsx
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Table } from "semantic-ui-react";
 
 import PilotsListHeader from "./PilotsListHeader";
 import PilotsListRow from "./PilotsListRow";
 
-export default class PilotsList extends Component {
-    render() {
-        const {pilots, onPilotClicked, currentPilot} = this.props;
+import orm from "app/orm";
 
-        const pilotRows = pilots.map(pilot => (
+import { selectPilot } from "../pilotsActions";
+import { selectCurrentPilot } from "../pilotsSelectors";
+
+const mapState = (state) => {
+    // Create a Redux-ORM Session from our "entities" slice, which
+    // contains the "tables" for each model type
+    const session = orm.session(state.entities);
+
+    // Retrieve the model class that we need.  Each Session
+    // specifically "binds" model classes to itself, so that
+    // updates to model instances are applied to that session.
+    // These "bound classes" are available as fields in the sesssion.
+    const {Pilot} = session;
+
+    // Query the session for all Pilot instances.
+    // The QuerySet that is returned from all() can be used to
+    // retrieve instances of the Pilot class, or retrieve the
+    // plain JS objects that are actually in the store.
+
+    // The withModels modifier will let us map over Model instances
+    // for each entry, rather than the plain JS objects.
+
+    // Extract a list of IDs for each Pilot entry
+    const pilots = Pilot.all().toModelArray().map(pilotModel => pilotModel.getId());
+
+    const currentPilot = selectCurrentPilot(state);
+
+    // Return the list of pilot IDs and the current pilot ID as props
+    return { pilots, currentPilot };
+}
+
+// Make an object full of action creators that can be passed to connect
+// and bound up, instead of writing a separate mapDispatch function
+const actions = {
+    selectPilot,
+};
+
+export class PilotsList extends Component {
+    render() {
+        const {pilots = [], selectPilot, currentPilot} = this.props;
+
+        const pilotRows = pilots.map(pilotID => (
             <PilotsListRow
-                pilot={pilot}
-                key={pilot.name}
-                onPilotClicked={onPilotClicked}
-                selected={pilot.id === currentPilot}
+                pilotID={pilotID}
+                key={pilotID}
+                onPilotClicked={selectPilot}
+                selected={pilotID === currentPilot}
                 />
         ));
 
@@ -27,3 +67,5 @@ export default class PilotsList extends Component {
         )
     }
 }
+
+export default connect(mapState, actions)(PilotsList);
