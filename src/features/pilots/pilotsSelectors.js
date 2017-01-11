8d7257762afec67eb3b9e4c697a7f7897a0af001
zyhou <richard.maxime9@gmail.com>
2017-01-11 10:31:30 +0100

    add logic to pilot is being edited

--- src/features/pilots/pilotsSelectors.js
+++ src/features/pilots/pilotsSelectors.js
@@ -5,4 +5,9 @@ export const selectPilots = state => state.pilots;
 export const selectCurrentPilot = createSelector(
     selectPilots,
     pilots => pilots.currentPilot,
+);
+
+export const selectIsEditingPilot = createSelector(
+    selectPilots,
+    pilots => pilots.isEditing
 ); 
